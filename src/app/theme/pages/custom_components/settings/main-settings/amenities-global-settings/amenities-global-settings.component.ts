ea697282f4cbf1ef84742ff42be89af2585c59b0
Abdul Wahab Shah <wahab.shah23@gmail.com>
2018-06-29 15:32:17 +0500

    29th June 3:30pm
    Amenities Settings Component

--- src/app/theme/pages/custom_components/settings/main-settings/amenities-global-settings/amenities-global-settings.component.ts
+++ src/app/theme/pages/custom_components/settings/main-settings/amenities-global-settings/amenities-global-settings.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AmenitiesGlobalSettingsComponent implements OnInit {
 
+    addAmenityForm = false;
+
+    isAmenityGridView = true;
+    AmenityViewName = 'List View';
+
   constructor() { }
 
   ngOnInit() {
   }
+
+    changeAmenityView() {
+        this.isAmenityGridView = !this.isAmenityGridView;
+        if (this.isAmenityGridView) {
+            this.AmenityViewName = 'List View';
+        }
+        else {
+            this.AmenityViewName = 'Grid View';
+        }
+        this.addAmenityForm = false;
+    }
 
 }
