1e712990b10c5ad825e351ddd178e5be85126a53
Abdul Wahab Shah <wahab.shah23@gmail.com>
2018-08-01 17:32:43 +0500

    1/8/2018
    Amenities Section Updated Grid view modified
    Styling updated
    Global Styles Added
    searchbox animation done

--- src/app/theme/pages/custom_components/settings/main-settings/amenities-global-settings/amenities-global-settings.component.ts
+++ src/app/theme/pages/custom_components/settings/main-settings/amenities-global-settings/amenities-global-settings.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import {Folder} from "../../../../../../models/Folder.model";
 import { AmenitiesService } from "../../../../../../services/amenities.service";
 
 @Component({
     selector: 'app-amenities-global-settings',
     templateUrl: './amenities-global-settings.html',
-    styleUrls: ['./amenities-global-settings.css']
+    styleUrls: ['./amenities-global-settings.css'],
+    animations:  [
+        trigger('slideRightLeft', [
+            state('show', style({
+                transform: 'translateX(0)',
+                // opacity: 1,
+                // display: 'inline-block'
+                width: '200px'
+            })),
+            state('hide', style({
+                transform: 'translateX(0)',
+                // opacity: 0,
+                // display: 'none'
+                width: '0px'
+            })),
+            transition('show => hide', animate('400ms ease-out')),
+            transition('hide => show', animate('400ms ease-in'))
+        ]),
+    ]
 })
 export class AmenitiesGlobalSettingsComponent implements OnInit {
 
@@ -19,9 +38,22 @@ export class AmenitiesGlobalSettingsComponent implements OnInit {
 
     Amenities;  // amenities property for amenity service implementation
 
+    //declare a variable to hold class name:
+    public display_searchBox = 'showSearch';
+
+    //variable to hold boolean value for search field
+    public isSearchBoxVisible = false;
+
     constructor( amenity_service: AmenitiesService) {
         // let amenity_service = new AmenitiesService();     //NOT GOOD APPROACH, IT MAKES IT TIGHTLY COUPLED TOO!
         this.Amenities = amenity_service.getAmenities();
+    }
+
+    searchBox:string = 'hide';
+
+    toggleSearchBox() {
+        // 1-line if statement that toggles the value:
+        this.searchBox = this.searchBox === 'hide' ? 'show' : 'hide';
     }
 
     ngOnInit() {
@@ -51,5 +83,16 @@ export class AmenitiesGlobalSettingsComponent implements OnInit {
     deleteAmenity(id){
         console.log(id);
         this.Amenities.splice(id,1);
+    }
+
+    //function to change the class from style1 to style 2 when clicked
+    toggle_class(){
+        // if(this.display_searchBox=="showSearch"){
+        //     this.display_searchBox='hideSearch';
+        // }else{
+        //     this.display_searchBox='showSearch';
+        // }
+
+        this.isSearchBoxVisible = !this.isSearchBoxVisible;
     }
 }
