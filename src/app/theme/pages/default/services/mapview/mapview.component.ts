aafdb4612fdc5b3796ce24e47db5bfdd284db36b
Muhammad Waleed <30799999+MuhammadWaleed83752@users.noreply.github.com>
2018-08-29 15:26:23 +0500

    29-Aug-2018
    Map Services Editing

--- src/app/theme/pages/default/services/mapview/mapview.component.ts
+++ src/app/theme/pages/default/services/mapview/mapview.component.ts
@@ -1,115 +1,133 @@
 import {
-    Component,
-    // ViewEncapsulation,
-    ViewChild,
-    AfterViewInit,
-    NgZone
+  Component,
+  // ViewEncapsulation,
+  ViewChild,
+  AfterViewInit,
+  NgZone
 } from "@angular/core";
 import { ScriptLoaderService } from "../../../../../_services/script-loader.service";
 // import { } from "googlemaps";
 import { AgmMap, MapsAPILoader } from "@agm/core";
 
-
 @Component({
-    templateUrl: "./mapview.component.html",
-    styleUrls: ["./mapview.component.css"]
+  templateUrl: "./mapview.component.html",
+  styleUrls: ["./mapview.component.css"]
 })
 export class MapViewServicesComponent implements AfterViewInit {
-    @ViewChild(AgmMap) agmMap: AgmMap;
+  @ViewChild(AgmMap)
+  agmMap: AgmMap;
 
-    //Map buttons Start
-    isMapDetail = false;
-    //Map button End
+  //Map buttons Start
+  isMapDetail = false;
+  //Map button End
 
-    //side pop-up
-    showServices = false;
-    showOrderHistory = false;
-    showInfo = false;
+  //side pop-up
+  showServices = false;
+  showOrderHistory = false;
+  showInfo = false;
+  showProducts = false;
+  showServicesInfo = false;
+  lang: any;
+  lat: any;
+  lati: number = 51.678418;
+  lng: number = 7.809007;
 
-    lang: any;
-    lat: any;
-    lati: number = 51.678418;
-    lng: number = 7.809007;
+  totalChecked = 0;
 
-    totalChecked = 0;
+  constructor(
+    private _script: ScriptLoaderService
+  ) // private mapsAPILoader: MapsAPILoader,
+  // private ngZone: NgZone,
+  {}
 
+  ngOnInit() {}
 
-    constructor(
-        private _script: ScriptLoaderService,
-        // private mapsAPILoader: MapsAPILoader,
-        // private ngZone: NgZone,
-    ) { }
+  ngAfterViewInit() {
+    this._script.loadScripts("app-services", [
+      "//www.amcharts.com/lib/3/plugins/tools/polarScatter/polarScatter.min.js",
+      "//www.amcharts.com/lib/3/plugins/export/export.min.js",
+      "assets/app/js/services.js"
+    ]);
+  }
 
-    ngOnInit() { }
+  adjustRadiusMap() {
+    setTimeout(() => {
+      this.agmMap.triggerResize();
+    }, 2000);
+  }
 
-    ngAfterViewInit() {
-        this._script.loadScripts("app-services", [
-            "//www.amcharts.com/lib/3/plugins/tools/polarScatter/polarScatter.min.js",
-            "//www.amcharts.com/lib/3/plugins/export/export.min.js",
-            "assets/app/js/services.js",
-        ]);
+  onSearchChecked(event) {
+    if (event.target.checked) {
+      this.totalChecked++;
+    } else {
+      this.totalChecked--;
     }
+  }
 
-    adjustRadiusMap() {
-        setTimeout(() => {
-            this.agmMap.triggerResize();
-        }, 2000);
+  sideInfoPop(value: string) {
+    if (value == "history") {
+      this.showServices = false;
+      this.showOrderHistory = true;
+      this.showInfo = false;
+      this.showProducts = false;
+      this.showServicesInfo = false;
+    } else if (value == "service") {
+      this.showServices = true;
+      this.showOrderHistory = false;
+      this.showInfo = false;
+      this.showProducts = false;
+      this.showServicesInfo = false;
+    } else if (value == "products") {
+      this.showServices = false;
+      this.showOrderHistory = false;
+      this.showInfo = false;
+      this.showProducts = true;
+      this.showServicesInfo = false;
+    } else if (value == "serviceInfo") {
+      this.showServices = false;
+      this.showOrderHistory = false;
+      this.showInfo = false;
+      this.showProducts = false;
+      this.showServicesInfo = true;
+    } else {
+      this.showServices = false;
+      this.showOrderHistory = false;
+      this.showInfo = true;
+      this.showProducts = false;
     }
-
-    onSearchChecked(event)
-    {
-       if(event.target.checked)
-       {
-           this.totalChecked++;
-       }
-       else
-       {
-           this.totalChecked--;
-       }
-    }
-
-    sideInfoPop(value: string) {
-        if (value == "history") {
-            this.showServices = false;
-            this.showOrderHistory = true;
-            this.showInfo = false;
-        } else if (value == "service") {
-            this.showServices = true;
-            this.showOrderHistory = false;
-            this.showInfo = false;
-        } else {
-            this.showServices = false;
-            this.showOrderHistory = false;
-            this.showInfo = true;
-        }
-    }
-     t= false;
-     deselectAll: boolean = false;
-    selectAll() {
-        this.deselectAll = !this.deselectAll;
-       this.t = this.deselectAll;
+  }
+  t = false;
+  deselectAll: boolean = false;
+  selectAll() {
+    this.deselectAll = !this.deselectAll;
+    this.t = this.deselectAll;
     //    console.log(this.t);
 
-    if(this.t)
-    {
-        this.totalChecked = 3;
+    if (this.t) {
+      this.totalChecked = 3;
+    } else {
+      this.totalChecked = 0;
     }
-    else
-    {
-        this.totalChecked = 0;
-    }
+  }
 
-    }
-    
-    checkProduct: boolean = true;
-    checkServices: boolean = true;
+  checkProduct: boolean = true;
+  checkServices: boolean = true;
 
-
-    filterProducts() {
+  filterProducts() {
     this.checkProduct = !this.checkProduct;
-    }
+    this.showServices = false;
+    this.showOrderHistory = false;
+    this.showInfo = false;
+    this.showProducts = false;
+    this.showServicesInfo = false;
+  }
 
-    filterServices() {
-        this.checkServices = !this.checkServices;
-        }
+  filterServices() {
+    this.checkServices = !this.checkServices;
+    this.showServices = false;
+    this.showOrderHistory = false;
+    this.showInfo = false;
+    this.showProducts = false;
+    this.showServicesInfo = false;
+  }
 }
