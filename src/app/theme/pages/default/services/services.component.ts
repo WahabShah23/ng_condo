142fec275de3a7e802e9c76a44277c46ca16a45e
Mudasser Lodhi <mudasserlodhi@yahoo.com>
2018-08-31 15:41:14 +0500

    31/08/2018
    New Calendar

--- src/app/theme/pages/default/services/services.component.ts
+++ src/app/theme/pages/default/services/services.component.ts
@@ -41,4 +41,45 @@ export class ServicesComponent implements OnInit, AfterViewInit {
             this.viewName = "Grid View";
         }
     }
+
+
+    //Calendar
+    months = ["January" , "February" , "March", "April" , "May" , "June" , "July" , "August" , "September" , "October" , "November" , "December"];
+    days = [{date:'01' , day:'Sun'},
+            {date:'02' , day:'Mon'},
+            {date:'03' , day:'Tue'},
+            {date:'04' , day:'Wed'},
+            {date:'05' , day:'Thur'},
+            {date:'06' , day:'Fri'},
+            {date:'07' , day:'Sat'}];
+    i=0;
+    selected = this.months[this.i]; 
+    
+    moveToRight()
+    {
+        if(this.i==11)
+        {
+            this.i=0;
+        }
+        else
+        {
+            this.i++;
+        }
+        this.selected = this.months[this.i];
+    }  
+
+
+    moveToLeft()
+    {
+        if(this.i==0)
+        {
+            this.i=11;
+        }
+        else
+        {
+            this.i--;
+        }
+        this.selected = this.months[this.i];   
+    }
+
 }
