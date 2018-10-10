dc4addcb7b0dc819f29120972f20da68ac3750b0
Abdul Wahab Shah <wahab.shah23@gmail.com>
2018-10-10 16:47:10 +0500

    Revert "Revert "10th Oct 2018""
    This reverts commit 45d34ed8765d9234382eb3b501650235212e5325.

--- src/app/theme/pages/custom_components/settings/main-settings/financial-settings-global/financial-digital-wallet/financial-digital-wallet.component.ts
+++ src/app/theme/pages/custom_components/settings/main-settings/financial-settings-global/financial-digital-wallet/financial-digital-wallet.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinancialDigitalWalletComponent implements OnInit {
 
+  cosigners : boolean = true;
   constructor() { }
 
   ngOnInit() {
